5a6c4bb92c0b7b3b39f9fd785dc4ad4afb09755e
rsl140 <rsl140@qq.com>
2017-10-22 01:19:08 +0800

    init

---
+++ testwechat/routes/wechat.js
@@ -0,0 +1,25 @@
+var express = require('express');  
+var router = express.Router();  
+var wechat = require('wechat');  
+  
+var config = {  
+  token : 'wechat',  
+  appid : 'wx53fb10f438bee5af',  
+  appsecret :'261a006bf239e0c7fd978883552a66e6',  
+  encodingAESKey : ''  
+};  
+  
+router.use(express.query());  
+  
+router.use('/', wechat(config, function(req, res, next) {  
+    console.log(req.weixin);  
+    var message = req.weixin;  
+    //文本  
+    if (message.Content === '1') {  
+  
+        res.reply('hehe');  
+    }  
+  
+}));  
+  
+module.exports = router;
