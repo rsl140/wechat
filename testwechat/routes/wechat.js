48aff8c5bc1dfa7cfc09df1885a3ff4d48f5e3c0
rsl140 <rsl140@qq.com>
2017-10-23 18:11:57 +0800

    add config.json

--- testwechat/routes/wechat.js
+++ testwechat/routes/wechat.js
@@ -1,17 +1,20 @@
 var express = require('express');  
 var router = express.Router();  
-var wechat = require('wechat');  
+var wechat = require('wechat');
+var configApi = require('../config.json');
   
-var config = {  
-  token : 'wechat',  
-  appid : 'wx53fb10f438bee5af',  
-  appsecret :'261a006bf239e0c7fd978883552a66e6',  
-  encodingAESKey : ''  
-};  
+var config = configApi;
+// var config = {  
+//   token : 'wechat',  
+//   appid : 'wx53fb10f438bee5af',  
+//   appsecret :'261a006bf239e0c7fd978883552a66e6',  
+//   encodingAESKey : ''  
+// };  
   
 router.use(express.query());  
   
 router.use('/', wechat(config, function(req, res, next) {  
+  console.log(config);
     console.log(req.weixin);  
     var message = req.weixin;  
     //文本  
