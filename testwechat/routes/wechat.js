05e1f7123efce2e63a588373bc390556e0d47a55
rsl140 <rsl140@qq.com>
2017-10-23 22:03:05 +0800

    修改config.json内容

--- testwechat/routes/wechat.js
+++ testwechat/routes/wechat.js
@@ -1,28 +1,43 @@
 var express = require('express');  
 var router = express.Router();  
 var wechat = require('wechat');
+var wechtApi = require('wechat-api');
 var configApi = require('../config.json');
   
 var config = configApi;
-// var config = {  
-//   token : 'wechat',  
-//   appid : 'wx53fb10f438bee5af',  
-//   appsecret :'261a006bf239e0c7fd978883552a66e6',  
-//   encodingAESKey : ''  
-// };  
   
 router.use(express.query());  
+var api = new WechatAPI(config.appID, config.appSecret);
   
 router.use('/', wechat(config, function(req, res, next) {  
   console.log(config);
-    console.log(req.weixin);  
-    var message = req.weixin;  
-    //文本  
-    if (message.Content === '1') {  
-  
-        res.reply('hehe');  
-    }  
-  
-}));  
+  console.log(req.weixin);  
+  var message = req.weixin;  
+  //文本  
+  if (message.Content === '1') {  
+
+      res.reply('hehe');  
+  }
+  var menu = {
+        "button": [
+            {
+                "type": "click", 
+                "name": "123", 
+                "key": "V1001_TODAY_MUSIC"
+            }
+        ]
+    }
+    api.createMenu(menu, function(err, result){
+     console.log(result);
+    });
+    
+}));
+
+
+
+
+// api.createMenu(menu, function (err, result) {
+//   console.log(result);
+// });
   
 module.exports = router;
